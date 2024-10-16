1a1630ef07306ffd80905a186a621d9adc72e678
Crassus-hue <GodCrassus@gmail.com>
2024-10-16 06:24:20 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "8295434323"
+global.ownername = "𝐆𝐨𝐝 𝐂𝐫𝐚𝐬𝐬𝐮𝐬"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '8295434323'  //creator number
+global.ownername = '𝐆𝐨𝐝 𝐂𝐫𝐚𝐬𝐬𝐮𝐬 //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
